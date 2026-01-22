452d30202f6d09b37196560259f60cad537606db
FabsSWD <Fabarcamonge@gmail.com>
2026-01-22 16:33:30 -0600

    feat: add ListClipboardHistory use case for retrieving clipboard history

--- src/lib.zig
+++ src/lib.zig
@@ -12,4 +12,5 @@ pub const domain = struct {
 
 pub const application = struct {
     pub const AddClipboardEntry = @import("application/add_clipboard_entry.zig").AddClipboardEntry;
+    pub const ListClipboardHistory = @import("application/list_clipboard_history.zig").ListClipboardHistory;
 };
